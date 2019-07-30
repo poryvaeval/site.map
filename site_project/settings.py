68c0ec63e018c4acbde3d9fec45d9b5928ecfa09
poryvaeval <kravenston@gmail.com>
2019-07-30 18:02:53 +0500

    heroku config

--- site_project/settings.py
+++ site_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'djeym',
+    'whitenoise.runserver_nostatic',
     
 ]
  #django-ckeditor
@@ -85,6 +86,7 @@ DJEYM_YMAPS_ICONS_FOR_CATEGORIES_JS = []
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -159,5 +161,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
